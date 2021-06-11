9ca96448a2e23b893e58aad92a1a488bc95155e9
Antiselekotin <deksazon8@gmail.com>
2021-06-11 13:05:24 +0300

    add timestamps to settings

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,4 +9,5 @@ class Setting extends CoreModel
     protected $table = 'setting';
     protected $fillable = ['name', 'title', 'description', 'header', 'h1', 'index_text', 'ya_metriks', 'google_metriks',
         'all_rev_title', 'all_rev_des', 'all_rev_h1', 'all_rev_text', 'contact_title', 'contact_des', 'ya_code', 'moderation'];
+    public $timestamps = false;
 }
